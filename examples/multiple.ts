540ecc6ee52a40f8554cce6b4189ef22cd6b3830
Luke Autry <lautry@netflix.com>
2022-02-28 01:46:05 -0500

    clean up examples

--- examples/multiple.ts
+++ examples/multiple.ts
@@ -5,11 +5,13 @@ cliParser(parse(Deno.args), (b) =>
   b.list("deno run examples/multiple.ts", {
     description: "This is a CLI with multiple commands",
     commands: (b) =>
-      b.command("first", {
-        description: "First command",
-        args: (a) => a.add("a", { type: "string" }).run(console.log),
-      }).command("second", {
-        description: "Second command",
-        args: (a) => a.add("b", { type: "string" }).run(console.log),
-      }),
+      b
+        .command("first", {
+          description: "First command",
+          args: (a) => a.add("a", { type: "string" }).run(console.log),
+        })
+        .command("second", {
+          description: "Second command",
+          args: (a) => a.add("b", { type: "string" }).run(console.log),
+        }),
   }));
--- examples/nested.ts
+++ examples/nested.ts
@@ -1,31 +1,35 @@
 import { cliParser } from "../mod.ts";
 import { parse } from "https://deno.land/std/flags/mod.ts";
 
-console.log(parse(Deno.args));
-
 cliParser(parse(Deno.args), (b) =>
-  b.list("deno run examples/nested.ts", {
-    description: "This is a CLI with multiple commands",
-    commands: (b) =>
-      b.list("first", {
-        description: "First command",
-        commands: (b) =>
-          b.command("second", {
-            description: "Second command",
-            args: (a) =>
-              a
-                .add("bus", { type: "string", alias: "b" })
-                .add("car", { type: "integer", optional: true }, [123, 567])
-                .add(
-                  "train",
-                  { type: "boolean" },
-                )
-                .run((v) => {
-                  console.log(v);
-                }),
-          }).command("third", {
-            description: "Third command",
-            args: (a) => a.add("b", { type: "string" }).run(console.log),
-          }),
-      }),
-  }));
+  b
+    .list("deno run examples/nested.ts", {
+      description: "This is a CLI with multiple commands",
+      commands: (b) =>
+        b.list("first", {
+          description: "First command",
+          commands: (b) =>
+            b
+              .command("second", {
+                description: "Second command",
+                args: (a) =>
+                  a
+                    .add("bus", { type: "string", alias: "b" })
+                    .add("car", { type: "integer", optional: true }, [
+                      123,
+                      567,
+                    ])
+                    .add("train", { type: "boolean" })
+                    .run((v) => {
+                      console.log(v);
+                    }),
+              })
+              .command("third", {
+                description: "Third command",
+                args: (a) =>
+                  a
+                    .add("b", { type: "string" })
+                    .run(console.log),
+              }),
+        }),
+    }));
